2bc6a13744033f67af33f0a4a66d9040ab965b04
Maxime Daniel <root@maxux.net>
2021-06-09 19:23:41 +0200

    client: fix const and comment non working code

--- soft-client/client.js
+++ soft-client/client.js
@@ -1,12 +1,13 @@
 const msgbus = require("./msgbus");
 
 const mb = msgbus.connect()
-mb.prepare("wallet.stellar.balance.tft", [1002], 0)
+mb.prepare("wallet.stellar.balance.tft", [10], 0)
 mb.send("GA7OPN4A3JNHLPHPEWM4PJDOYYDYNZOM7ES6YL3O7NC3PRY3V3UX6ANM")
-const values = mb.read()
+let values = mb.read()
 
 console.log(values)
 
+/*
 mb.prepare("griddb.twins.create", [1002], 0)
 mb.send("some_peer_id")
 values = mb.read()
@@ -18,3 +19,4 @@ mb.send(1)
 values = mb.read()
 
 console.log(values)
+*/
