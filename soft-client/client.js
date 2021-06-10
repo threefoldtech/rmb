8ee99cbcdde213139a41e1694e5f38702c0284f3
dylanVerstraete <dylan_verstraete1@hotmail.com>
2021-06-10 14:24:48 +0200

    refactor and cleanup code

--- soft-client/client.js
+++ soft-client/client.js
@@ -1,14 +1,13 @@
-const msgbus = require("./msgbus")
+const { MessageBusClient } = require('./msgbus')
 
-const mb = msgbus.connect("", 6379)
+const mb = new MessageBusClient(6379)
+
 const message = mb.prepare("wallet.stellar.balance.tft", [12], 0, 2)
 mb.send(message, "GA7OPN4A3JNHLPHPEWM4PJDOYYDYNZOM7ES6YL3O7NC3PRY3V3UX6ANM")
 mb.read(message, function (result) {
   console.log("result received")
   console.log(result)
 })
-
-
 
 // mb.prepare("griddb.twins.create", [1002], 0)
 // mb.send("some_peer_id")
