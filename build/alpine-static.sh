992b5d0bccd41d689fb52e460bde8f316468067d
Maxime Daniel <root@maxux.net>
2021-06-16 23:28:43 +0200

    build: add alpine static build script

---
+++ build/alpine-static.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -ex
+
+cd /
+
+apk add alpine-sdk
+
+git clone https://github.com/vlang/v
+cd v && make
+
+cd /
+export PATH=/v:$PATH
+
+mkdir /root/.vmodules/despiegk
+mkdir /root/.vmodules/threefoldtech
+
+git clone https://github.com/threefoldtech/vgrid /root/.vmodules/threefoldtech/vgrid
+git clone https://github.com/threefoldtech/rmb /root/.vmodules/threefoldtech/rmb
+git clone https://github.com/crystaluniverse/crystallib /root/.vmodules/despiegk/crystallib
+
+cd /root/.vmodules/threefoldtech/rmb/msgbusd
+v -cc gcc -cflags -static msgbusd.v
+strip -s msgbusd
