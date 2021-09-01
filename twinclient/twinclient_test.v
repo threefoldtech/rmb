ac9381660779b98e18771fe223a962bb2b764bc5
MElborolossy <mohammedelborolossy@gmail.com>
2021-09-01 12:50:24 +0200

    Update twinclient test

--- twinclient/twinclient_test.v
+++ twinclient/twinclient_test.v
@@ -1,7 +1,7 @@
 import threefoldtech.rmb.twinclient
 
 fn test_twin_client() {
-	mut twin :=  client.new('localhost:6379', 49) or {panic("Can't connect to redis server with error $err")}
+	mut twin :=  twinclient.new('localhost:6379', 49) or {panic("Can't connect to redis server with error $err")}
 
 	mut msg := twin.send("twinserver.twins.get", '{"id": 49}') or {panic("Can't send msg with error $err")}
 	response := twin.read(msg)
